a858dfdc92b1fcfd0668497022dacb843a2d6c84
Miguel Ángel Monzón Martínez <miguelmonzon.inf@gmail.com>
2024-09-29 11:23:02 -0300

    M7D10 desafio: Added signup and login routes, removed TheWelcome component from HomeView

--- src/router/index.js
+++ src/router/index.js
@@ -16,6 +16,16 @@ const router = createRouter({
       // this generates a separate chunk (About.[hash].js) for this route
       // which is lazy-loaded when the route is visited.
       component: () => import('../views/AboutView.vue')
+    },
+    {
+      path: "signup",
+      name: "signup",
+      component: () => import('../views/SignupView.vue')
+    },
+    {
+      path: "login",
+      name: "login",
+      component: () => import('../views/LoginView.vue')
     }
   ]
 })
