fa1bd1511c9a81fc0f783090bc6b177214197be3
Miguel Ángel Monzón Martínez <miguelmonzon.inf@gmail.com>
2024-09-29 13:23:15 -0300

    M7D10 desafio: Updated .gitignore to ignore .env file and refactored firebaseConfig.js to use environment variables

--- src/firebaseConfig.js
+++ src/firebaseConfig.js
@@ -1,17 +1,15 @@
 // Import the functions you need from the SDKs you need
 import { initializeApp } from "firebase/app";
-import { getAuth } from "firebase/auth"
-// TODO: Add SDKs for Firebase products that you want to use
-// https://firebase.google.com/docs/web/setup#available-libraries
+import { getAuth } from "firebase/auth";
 
-// Your web app's Firebase configuration
+// Your web app's Firebase configuration using environment variables
 const firebaseConfig = {
-    apiKey: "AIzaSyC5e8kl9L5w7DEHx-XgNZayD1ix6npwLPo",
-    authDomain: "desafio-auth-b5d84.firebaseapp.com",
-    projectId: "desafio-auth-b5d84",
-    storageBucket: "desafio-auth-b5d84.appspot.com",
-    messagingSenderId: "857079476634",
-    appId: "1:857079476634:web:f4a1086c156181e1de4a23"
+    apiKey: import.meta.env.VITE_API_KEY,
+    authDomain: import.meta.env.VITE_AUTH_DOMAIN,
+    projectId: import.meta.env.VITE_PROJECT_ID,
+    storageBucket: import.meta.env.VITE_STORAGE_BUCKET,
+    messagingSenderId: import.meta.env.VITE_MESSAGING_SENDER_ID,
+    appId: import.meta.env.VITE_APP_ID,
 };
 
 // Initialize Firebase
