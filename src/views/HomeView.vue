9975f00afd40e4d2c4212407fd0f8da05cdb7485
Miguel Ángel Monzón Martínez <miguelmonzon.inf@gmail.com>
2024-09-29 13:17:37 -0300

    M7D10 desafio: Update HomeView.vue: add CSS classes and paragraphs to main element

--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -2,7 +2,14 @@
 </script>
 
 <template>
-  <main>
-    <h1>Home View</h1>
+  <main class="d-flex justify-content-center align-items-center vh-100 text-center">
+    <div>
+      <h1>Home View</h1>
+      <p class="mt-3">✨ Bienvenido a tu aplicación Vue.js! ✨</p>
+      <p>Recuerda que cada línea de código que escribes te acerca un paso más a tus objetivos</p>
+      <p>¡Sigue aprendiendo y creando! 🚀</p>
+    </div>
   </main>
 </template>
+
+<style scoped></style>
